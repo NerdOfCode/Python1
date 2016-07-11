c050e9161b9c25bf02fcffad03352bf46cd4302a
Nate <lovelaptops18@gmail.com>
2016-07-11 17:21:25 -0400

    worked

--- AI/bored.py
+++ AI/bored.py
@@ -1,8 +1,8 @@
 
-print "I am too!"
+print "I am too, whats your favorite number?"
 x = 18
 x = raw_input() 
 if x:
-  print "Thats my favorite number too! \ -Nate"
+  print "Thats my favorite number too! | -Nate"
 else:
   print "That must be Gy's favorite number"
