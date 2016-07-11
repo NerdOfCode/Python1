0b033c253e49443654881ee07c25ce1a772e53be
NerdOfLinux <golf@runningwind.com>
2016-07-11 16:37:48 -0500

    Update bored.py

--- AI/bored.py
+++ AI/bored.py
@@ -4,6 +4,6 @@ userinput = raw_input("Your favorite number is ")
 userinput=int(userinput)
 
 if userinput < 100:
-  print "That number is below 100"
+  print "That number is below 100."
 else:
   print "That is one big number!"
