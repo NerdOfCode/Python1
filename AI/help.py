18fe187d59af7f9a6bb4c758c2309f2ad153ac21
NerdOfLinux <golf@runningwind.com>
2016-07-11 16:12:19 -0500

    Update help.py

--- AI/help.py
+++ AI/help.py
@@ -3,6 +3,6 @@ from os import listdir
 print("Below are the possible commands:")
 pwd=os.getcwd()
 ls=os.listdir(pwd)
-for file in ls:
+for file in ls and file != "data.dat":
    file=file.replace(".py","")
    print file
