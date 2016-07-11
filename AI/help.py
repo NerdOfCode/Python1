0c86a2c7fc74564d1847d4aae397b0d30b03d08b
NerdOfLinux <golf@runningwind.com>
2016-07-11 16:09:35 -0500

    Update help.py

--- AI/help.py
+++ AI/help.py
@@ -1,7 +1,7 @@
 import os
 from os import listdir
 print("Below are the possible commands:")
-directory=os.popen("pwd")
+pwd=os.popen("pwd")
 ls=os.listdir(pwd)
 for file in ls:
    file=file.replace(".py","")
