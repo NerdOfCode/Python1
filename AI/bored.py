d6b1c328723531fb9622407b9801359802a6beff
NerdOfLinux <golf@runningwind.com>
2016-07-11 16:32:28 -0500

    Update bored.py

--- AI/bored.py
+++ AI/bored.py
@@ -2,11 +2,8 @@ X
 print "I am too, whats your favorite number?"
 
 input = raw_input() 
-number = 100
 
-
-
-if input > number:
+if input < 100:
   print "That number is below 100"
 else:
   print "That is one big number!"
