6ff19870021e46064077abd20053a09f10c3d93c
NerdOfLinux <golf@runningwind.com>
2016-07-11 16:28:35 -0500

    Update terminal.py

--- AI/terminal.py
+++ AI/terminal.py
@@ -3,7 +3,7 @@ with open('data.dat', 'r') as datafile:
 	name=datafile.read()
 pwd=os.getcwd()
 while True:
-	command=raw_input("%s:%s$ " %name %pwd)
+	command=raw_input("%s:%s$ " % (name, pwd))
 	if command == "exit":
 		break
 	else:
