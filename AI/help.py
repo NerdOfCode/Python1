7c64dda586fcbcb59402c303c25e35b1c224af74
NerdOfLinux <golf@runningwind.com>
2016-07-11 16:15:08 -0500

    Update help.py

--- AI/help.py
+++ AI/help.py
@@ -4,7 +4,7 @@ print("Below are the possible commands:")
 pwd=os.getcwd()
 ls=os.listdir(pwd)
 for file in ls:
-   if file == "data.dat":
+   if file == "data.dat" or file == "AI.py":
       continue
    file=file.replace(".py","")
    print file
