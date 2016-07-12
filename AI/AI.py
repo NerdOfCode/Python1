501a890b20f43d0dffdfb48531361e3378df7d94
NerdOfLinux <golf@runningwind.com>
2016-07-12 11:08:22 -0500

    Update AI.py

--- AI/AI.py
+++ AI/AI.py
@@ -14,6 +14,7 @@ if datacheck == 1:
 		command=raw_input("Command: ")
 		#Check if the command has a program
 		programcheck=os.path.isfile("%s.py" %command)
+		shellcheck=os.path.isfile("%s.sh" %command)
 		#Replace space with backslash and space
 		command = command.replace(" ", "\\ ")
 		#If there is a program then
@@ -21,14 +22,25 @@ if datacheck == 1:
 			#Run the program using os.system
 			os.system("python %s.py" %command)
 			continue
+		elif shellcheck == 1:
+			os.system("bash %s.sh)
+			continue
 		else:
 			#Ask the user is they would like to make a program
 			print("Sorry, I cant do that yet.")
 			print("Would you like to make a program for that y)es or n)o?")
 			makefile=raw_input()
 			if makefile == "y":
-				os.system("nano %s.py" %command)
-				continue
+				shell=raw_input("Would you like it to be a s)hell script or p)ython?")
+				if shell == "s":
+					os.system("nano %s.sh" %command)
+					continue
+				elif shell == "p":
+					os.system("nano %s.py" %command)
+					continue
+				else:
+					print("Error, please try again.")
+					continue
 			else:
 				print("Ok")
 				continue
