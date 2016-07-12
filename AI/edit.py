e917cf926d4107a1c4d87a046a08e2d44d90dfcb
NerdOfLinux <golf@runningwind.com>
2016-07-12 11:12:31 -0500

    Update edit.py

--- AI/edit.py
+++ AI/edit.py
@@ -1,6 +1,6 @@
 import os
-print("What program would you like to change?")
+print("What program would you like to change(complete filename)?")
 os.system("ls")
 file=raw_input()
 file=file.replace(" ", "\\ ")
-os.system("nano %s.py" %file)
+os.system("nano %s" %file)
