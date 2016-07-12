a54bc4063d1ffc06553915e8aad664ff8802a24e
NerdOfLinux <golf@runningwind.com>
2016-07-12 16:46:15 -0500

    Update terminal.py

--- AI/terminal.py
+++ AI/terminal.py
@@ -1,9 +1,5 @@
 import os
-with open('.data.dat', 'r') as datafile:
-	data=datafile.read()
-name=raw_input("Which user are you (full user name)?: ")
-if name in data and os.path.isfile(".%s.dat" %name):
-	name=name
+from AI.py import name
 pwd=os.getcwd()
 while True:
 	command=raw_input("%s:%s$ " % (name, pwd))
