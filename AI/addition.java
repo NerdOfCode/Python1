22b769b4577546b867c49cf51b328dd727948c50
Nate <lovelaptops18@gmail.com>
2016-07-12 17:16:10 -0400

    multiply.java now working

---
+++ AI/addition.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class addition {
+   public static void main(String[] args) {
+      Scanner s = new Scanner(System.in);
+      System.out.print("Enter first number: ");
+      int firstNumber = s.nextInt();
+      System.out.print("Enter second number: ");
+      int secondNumber = s.nextInt();
+      int sum = firstNumber + secondNumber;
+      System.out.println("The result of addition was " + sum);
+   }
+} 
