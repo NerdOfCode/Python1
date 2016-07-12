41bae6f90e48ae8c72ec58a379e3e08fa8cf99ea
NerdOfLinux <golf@runningwind.com>
2016-07-12 17:05:49 -0500

    Update terminal.py

--- AI/terminal.py
+++ AI/terminal.py
@@ -1,7 +1,9 @@
 import os
+import sys
+name=sys.argv[1]
 pwd=os.getcwd()
 while True:
-	command=raw_input("%s$ " %pwd)
+	command=raw_input("%s:%s$ " %(name, pwd))
 	if command == "exit":
 		break
 	else:
