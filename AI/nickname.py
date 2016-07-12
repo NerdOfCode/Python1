d437f61f0f7904ba56a96ea3121974a5fbe9cd34
Nate <lovelaptops18@gmail.com>
2016-07-12 09:30:16 -0400

    Auto1

--- AI/nickname.py
+++ AI/nickname.py
@@ -6,10 +6,10 @@ datacheck=os.path.isfile("nick.data")
 
 if datacheck == 1:
  with open('nick.data', 'r') as datafile:
-                name=datafile.read()
+  name=datafile.read()
 print "Hello %s, you already are registered with your nickname!" %name
-else:
-print "I dont know currently know your nickname!"
-print "Enter it now: " 
-name = raw_input()
-print "Saving %s, as your nickname!" %name
+   else:
+        print "I dont know currently know your nickname!"
+        print "Enter it now: " 
+        name = raw_input()
+        print "Saving %s, as your nickname!" %name
