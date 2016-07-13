2824aa0cfc62f84e2b9ea917d3a20982459e2b98
NerdOfLinux <golf@runningwind.com>
2016-07-13 11:06:48 -0500

    Update .owners.sh

--- AI/.owners.sh
+++ AI/.owners.sh
@@ -11,7 +11,6 @@ exit
 fi
 echo "Please enter your owners id"
 read pass
-<<<<<<< HEAD
 if [ $pass = '1812' ]  
 then
 echo "Welcome Nathan"
@@ -24,7 +23,6 @@ if [ $pass = '78447e5802a3ca1b35653282d9d53589' ]
 then
 echo "Welcome Nathan"
 elif [ $pass = 'efb632bb211e02dc56d4be50774c70df' ]
->>>>>>> ead6ad07b46ed9b30ce960b5bf1c2c3938bf3d26
 then
 echo "Welcome Gy"
 else
