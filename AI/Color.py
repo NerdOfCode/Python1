1e5fa40f6ecda792c8db6594a978c7fd9674aa78
NerdOfLinux <golf@runningwind.com>
2016-07-13 14:45:52 -0500

    Update Color.py

--- AI/Color.py
+++ AI/Color.py
@@ -3,11 +3,12 @@ name=sys.argv[1]
 with open('.%s.dat' %name, 'r') as datafile:
 	data=datafile.read()
 	lines=datafile.readlines()
+print(data)
 if "Favorite Color: " in data:
   data=lines[1]
   color=data.split("16",1)[1]
   print("Hello, %s, your favorite color is: %s" %(name, color))
-else:
+elif "Favorite Color: " not in data:
   print("Hello, %s." %name)
   color=raw_input("What is your favorite color?: ")
   with open('.%s.dat' %name, 'w') as datafile:
