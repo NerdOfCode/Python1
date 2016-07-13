daf5da8e46e61b282cbe5ea720f223b8744456c2
NerdOfLinux <golf@runningwind.com>
2016-07-13 15:09:45 -0500

    Update Color.py

--- AI/Color.py
+++ AI/Color.py
@@ -9,7 +9,7 @@ if "Favorite Color:" in data:
     	break
   print(line1)
   color=line1.split("Color: ", 1)[1]
-  print("Hello, %s, your favorite color is: %s" %(name, color))
+  print("Hello, %s, your favorite color is %s" %(name, color))
 elif "Favorite Color:" not in data:
   print("Hello, %s." %name)
   color=raw_input("What is your favorite color?: ")
