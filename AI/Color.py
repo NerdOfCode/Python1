3d2fd4cad58bbceea9bd4f4064fc4d169a83faf5
NerdOfLinux <golf@runningwind.com>
2016-07-13 14:33:10 -0500

    Update Color.py

--- AI/Color.py
+++ AI/Color.py
@@ -1,3 +1,8 @@
 name=sys.argv[1]
+with open('%s.dat' %name, 'r') as datafile:
+  datafile.write("Favortie Color: ")
+  
 print("Hello, %s." %name)
-color=raw_input("What ")
+color=raw_input("What is your favorite color?: ")
+with open('%s.dat' %name, 'w') as datafile:
+  datafile.write("Favortie Color: ")
