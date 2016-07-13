12bab56e5851f9d232487e1c649bdb926c2ee54e
NerdOfLinux <golf@runningwind.com>
2016-07-13 11:09:37 -0500

    Update .owners.sh

--- AI/.owners.sh
+++ AI/.owners.sh
@@ -4,37 +4,36 @@ echo "You are about to access the restricted owners panel (y-yes n-no)"
 read ans
 if [ $ans = 'y' ]
 then
-echo "Okay"
+  echo "Okay"
 else
-echo "goodbye"
-exit
+  echo "goodbye"
+  exit
 fi
 echo "Please enter your owners id"
 read pass
 if [ $pass = '1812' ]  
 then
-echo "Welcome Nathan"
+  echo "Welcome Nathan"
 
 elif [ $pass = '1218' ]
 
 pass=$(echo $pass | md5)
 if [ $pass = '78447e5802a3ca1b35653282d9d53589' ] 
 then
-echo "Welcome Nathan"
+  echo "Welcome Nathan"
 elif [ $pass = 'efb632bb211e02dc56d4be50774c70df' ]
 then
-echo "Welcome Gy"
-else
-echo "Incorrect"
-exit 
-
-  fi
+  echo "Welcome Gy"
+  else
+  echo "Incorrect"
+  exit 
+fi
 echo "What would you like to view?"
 read view
 if [ $view = 'users' ]
 then
-echo $1
-else 
-echo "No support"
-exit
+  echo $1
+  else 
+  echo "No support"
+  exit
 fi
