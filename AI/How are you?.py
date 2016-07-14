7de5a1b8c3429b1fc2646413e3be70a963daf50b
NerdOfLinux <golf@runningwind.com>
2016-07-14 13:50:29 -0500

    Update How are you?.py

--- AI/How are you?.py
+++ AI/How are you?.py
@@ -1,4 +1,10 @@
-with open('data.dat', 'r') as datafile:
-	name=datafile.read()
+import sys
+import random
+name=sys.argv[1]
 feeling=raw_input("How are you today, %s?: " %name)
-print("I'm also feeling %s" %feeling)
+feelings=['good', 'happy', 'sad', 'cheerful']
+random=random.choice(feelings)
+if feeling.lower() == random.lower():
+	print("I am also feeling %s right now." %random)
+else:
+	print("Oh. I am feeling %s right now." %random)
