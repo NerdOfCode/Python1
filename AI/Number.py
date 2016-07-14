a09b6a641cbe74900e21f47e0f71da37263bef95
NerdOfLinux <golf@runningwind.com>
2016-07-14 11:23:50 -0500

    Update Number.py

--- AI/Number.py
+++ AI/Number.py
@@ -2,14 +2,14 @@ import sys
 name=sys.argv[1]
 with open('.%s.dat' %name, 'r') as datafile:
         data=datafile.read()
-if "Favorite Number: " in data:
+if "Favorite Number:" in data:
   for line in data.splitlines():
     if "Favorite Number: " in line:
         line1=line
         break
   number=line1.split("Favorite Number: ", 1)[1]
   print("Hello, %s, your favorite number is %s" %(name, number))
-elif "Favorite Number: " not in data:
+elif "Favorite Number:" not in data:
   print("Hello, %s." %name)
   number=raw_input("What is your favorite Number?: ")
   with open('.%s.dat' %name, 'a') as datafile:
