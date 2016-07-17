79fe9005e7251951cb870084affe0f61ff38afb8
NerdOfLinux <golf@runningwind.com>
2016-07-17 18:19:42 -0500

    Update Setup.sh

--- AI/Setup.sh
+++ AI/Setup.sh
@@ -1,4 +1,4 @@
 curl -O https://pypi.python.org/packages/5b/db/45ec294a1776732b80118660ba13389f8178461273e1214d2ae604fa65a7/pyttsx-1.1.tar.gz
 tar -xzvf pyttsx-1-1.tar.gz
-cd pyttsx-1-1.tar.gz
-python setup.py --user
+cd pyttsx-1-1
+python setup.py install --user
