4bacb9b4f941d80601cc2a2de77ebbd9869cf802
NerdOfLinux <golf@runningwind.com>
2016-07-17 16:14:02 -0500

    Update AI.py

--- AI/AI.py
+++ AI/AI.py
@@ -66,7 +66,7 @@ if datacheck == 1:
                         os.system("./a.out")
                         os.remove("a.out")
                 elif ccheck == 1:
-                	os.system("gcc -o %s %s.c" %command)
+                	os.system("gcc -o %s %s.c" %(command, command))
                 	os.system("./%s" %command)
                 	os.remove("%s" %command)
                         
