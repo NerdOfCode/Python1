afbe75ccf1449b96b6dc41064a79b0293f786200
NerdOfLinux <golf@runningwind.com>
2016-07-17 19:17:04 -0500

    Update Setup.sh

--- AI/Setup.sh
+++ AI/Setup.sh
@@ -1,3 +1,6 @@
+curl -O https://bootstrap.pypa.io/ez_setup.py
+python ez_setup.py --user
+rm ez_setup.py
 curl -O https://pypi.python.org/packages/5b/db/45ec294a1776732b80118660ba13389f8178461273e1214d2ae604fa65a7/pyttsx-1.1.tar.gz
 tar -xzvf pyttsx-1.1.tar.gz
 cd pyttsx-1.1
